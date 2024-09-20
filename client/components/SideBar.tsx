70cb9994a03f8d88147145e1f36633624e78655e
peta-skr <atu.cho.33@gmail.com>
2024-09-20 21:03:14 +0900

    20240920

--- client/components/SideBar.tsx
+++ client/components/SideBar.tsx
@@ -1,15 +1,24 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useSession } from "next-auth/react";
 
 const SideBar = () => {
+  const session = useSession();
+
   const getData = async () => {
-    const userList = await axios.get("http://localhost:4000/user/all?userid=" + userid");
-    return userList;
+    console.log(session);
+
+    // const userList = await axios.get(
+    //   "http://localhost:4000/user/all?userid=" + userid
+    // );
+    // return userList;
   };
 
   useEffect(() => {
+    console.log(session.data?.user);
+
     const userList = getData();
-  }, []);
+  }, [session]);
 
   return <div className="w-1/5 h-full bg-cyan-400">test</div>;
 };
